0fe22293601e92c44d51639e89a74889fc0ed398
NurasylSagi <pushkin090705@gmail.com>
2025-03-06 21:31:16 +0500

    replace pasword

--- core/urls.py
+++ core/urls.py
@@ -27,3 +27,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # Подключаем маршруты из core
 ]
+
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
